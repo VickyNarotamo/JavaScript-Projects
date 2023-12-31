bb05dba1993bbf659edd55157e1dc3157167ebf3
VickyNarotamo <vickynarotamo18@gmail.com>
2023-12-31 22:32:54 +0000

    #30 #31
    #30 Include phone number field in form
    and when field is empty let a pop up warning message show up.
    #31 create a popup contact form

---
+++ Basic JavaScript Projects/Challenge #30/JS/Challege #30.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+
+    if (name === '' || email === '' || phone === '') {
+        alert('Please fill out all fields.');
+        return false;
+    }
+
+    return true;
+}
